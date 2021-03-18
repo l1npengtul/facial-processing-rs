ffb5f9904e37b0777f51edc0999c44bf07b0d79c
l1npengtul <l1npengtul@protonmail.com>
2021-03-18 17:49:14 +0900

    Add FacialProcessor struct

--- src/detector.rs
+++ src/detector.rs
@@ -1,8 +1,12 @@
+use crate::enumerate_devices::device::DevicePointer;
 use tvm::{Context, DeviceType};
 
 pub struct FacialProcessor {
     tvm_ctx: Context,
     tvm_ctx_dev_type: DeviceType,
     tvm_ctx_dev_id: i32,
-
+    #[cfg(feature = "vulkan", feature = "cl3")]
+    tvm_dev_ptr: DevicePointer,
 }
+
+impl FacialProcessor {}
