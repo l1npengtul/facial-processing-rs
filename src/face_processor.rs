be1edaf0f0a6b3588eecf609deea533097215cf0
l1npengtul <l1npengtul@protonmail.com>
2021-04-21 14:45:12 +0900

    fix ambiguous error

--- src/face_processor.rs
+++ src/face_processor.rs
@@ -164,7 +164,7 @@ impl FaceProcessorBuilder {
         let pnp = PnPSolver::new(
             Point2D::new(self.input_image_x as f64, self.input_image_y as f64),
             None,
-            Self::PnPArguments::NoRandsc,
+            PnPArguments::NoRandsc,
         )
         .unwrap();
 
